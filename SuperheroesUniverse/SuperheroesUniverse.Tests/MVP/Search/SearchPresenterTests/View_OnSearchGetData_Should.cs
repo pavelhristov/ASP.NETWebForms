84ba24cb01bf9d02661e14aead6c0787cb7b813b
pavelhristov <pavel.hristov_@abv.bg>
2017-02-21 21:47:33 +0200

    mvp tests

---
+++ SuperheroesUniverse/SuperheroesUniverse.Tests/MVP/Search/SearchPresenterTests/View_OnSearchGetData_Should.cs
@@ -0,0 +1,59 @@
+﻿using Moq;
+using NUnit.Framework;
+using SuperheroesUniverse.MVP.Search;
+using SuperheroesUniverse.Services;
+using SuperheroesUniverse.Tests.Helpers;
+using System.Linq;
+
+namespace SuperheroesUniverse.Tests.MVP.Search.SearchPresenterTests
+{
+    [TestFixture]
+    public class View_OnSearchGetData_Should
+    {
+        [Test]
+        [TestCase("3")]
+        [TestCase("name 3")]
+        [TestCase("")]
+        [TestCase("url")]
+        public void AddSuperheroToViewModel_WhenOnSearchGetDataIsRaised(string pattern)
+        {
+            // Arrange
+            var superheroes = Helper.GetSuperheroes();
+            var viewMock = new Mock<ISearchView>();
+            var expectedResults = superheroes.Where(sh => (sh.SecretIdentity.Contains(pattern) || sh.Name.Contains(pattern)) && !sh.isDeleted).AsQueryable();
+            viewMock.Setup(v => v.Model).Returns(new SearchViewModel());
+            var superheroesServiceMock = new Mock<ISuperheroesService>();
+            superheroesServiceMock.Setup(s => s.Search(pattern)).Returns(expectedResults);
+
+            SearchPresenter superheroesPresenter = new SearchPresenter(viewMock.Object, superheroesServiceMock.Object);
+
+            // Act
+            viewMock.Raise(v => v.OnSearchGetData += null, new SearchGetDataEventArgs(pattern));
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedResults, viewMock.Object.Model.Superheroes);
+        }
+
+        [Test]
+        [TestCase("whatever")]
+        [TestCase("")]
+        public void CallSearchWithProperParameters_WhenRaised(string pattern)
+        {
+            // Arrange
+            var superheroes = Helper.GetSuperheroes();
+            var viewMock = new Mock<ISearchView>();
+            var expectedResults = superheroes.Where(sh => (sh.SecretIdentity.Contains(pattern) || sh.Name.Contains(pattern)) && !sh.isDeleted).AsQueryable();
+            viewMock.Setup(v => v.Model).Returns(new SearchViewModel());
+            var superheroesServiceMock = new Mock<ISuperheroesService>();
+            superheroesServiceMock.Setup(s => s.Search(pattern)).Returns(expectedResults);
+
+            SearchPresenter superheroesPresenter = new SearchPresenter(viewMock.Object, superheroesServiceMock.Object);
+
+            // Act
+            viewMock.Raise(v => v.OnSearchGetData += null, new SearchGetDataEventArgs(pattern));
+
+            // Assert
+            superheroesServiceMock.Verify(s => s.Search(pattern), Times.Once());
+        }
+    }
+}
---
+++ SuperheroesUniverse/SuperheroesUniverse.Tests/MVP/SuperheroDetails/SuperheroDetailsPresenterTests/View_OnSuperheroDetailsGetData_Should.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using NUnit.Framework;
+using SuperheroesUniverse.Data.Models;
+using SuperheroesUniverse.MVP.SuperheroDetails;
+using SuperheroesUniverse.Services;
+using System;
+
+namespace SuperheroesUniverse.Tests.MVP.SuperheroDetails.SuperheroDetailsPresenterTests
+{
+    [TestFixture]
+    public class View_OnSuperheroDetailsGetData_Should
+    {
+        [Test]
+        public void AddSuperheroToViewModel_WhenOnSuperheroDetailsGetDataIsRaised()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            var superhero = new Superhero()
+            {
+                Id = id,
+                Name = "name",
+                SecretIdentity = "secret identity",
+                ImgUrl = "url",
+                isDeleted = false
+            };
+            var viewMock = new Mock<ISuperheroDetailsView>();
+            viewMock.Setup(v => v.Model).Returns(new SuperheroDetailsViewModel());
+            var superheroesServiceMock = new Mock<ISuperheroesService>();
+            superheroesServiceMock.Setup(s => s.GetById(id)).Returns(superhero);
+
+            SuperheroDetailsPresenter superheroesPresenter = new SuperheroDetailsPresenter(viewMock.Object, superheroesServiceMock.Object);
+
+            // Act
+            viewMock.Raise(v => v.OnSuperheroDetailsGetData += null, new SuperheroDetailsGetDataEventArgs(id));
+
+            // Assert
+            Assert.AreSame(superhero, viewMock.Object.Model.Superhero);
+        }
+
+        [Test]
+        public void CallGetByIdWithProperParameters_WhenRaised()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            var superhero = new Superhero()
+            {
+                Id = id,
+                Name = "name",
+                SecretIdentity = "secret identity",
+                ImgUrl = "url",
+                isDeleted = false
+            };
+            var viewMock = new Mock<ISuperheroDetailsView>();
+            viewMock.Setup(v => v.Model).Returns(new SuperheroDetailsViewModel());
+            var superheroesServiceMock = new Mock<ISuperheroesService>();
+            superheroesServiceMock.Setup(s => s.GetById(id)).Returns(superhero);
+
+            SuperheroDetailsPresenter superheroesPresenter = new SuperheroDetailsPresenter(viewMock.Object, superheroesServiceMock.Object);
+
+            // Act
+            viewMock.Raise(v => v.OnSuperheroDetailsGetData += null, new SuperheroDetailsGetDataEventArgs(id));
+
+            // Assert
+            superheroesServiceMock.Verify(s => s.GetById(id), Times.Once());
+        }
+    }
+}
